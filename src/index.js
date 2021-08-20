d4743fce8f8a5b1a37fc7e1a386063709962fe12
GintakaSakata <thesoonafu@gmail.com>
2021-08-21 00:47:28 +0700

    configMVCModel

--- src/index.js
+++ src/index.js
@@ -5,26 +5,28 @@ const app = express()
 const handlebars = require('express-handlebars')
 const port = 500
 
+const route = require('./routes/index.js');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 //http logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 //engine handle bars
 app.engine('handlebars', handlebars({
   extname: '.handlebars'
 }));
+
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/home', (req, res) => {
-  res.render('home'); 
-})
-
-app.get('/news', (req, res) => {
-  res.render('news'); 
-})
+//route
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ src/routes/site.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const route = express.Router();
+
+const sitesController = require('../app/controllers/SitesController.js');
+
+route.use('/home', sitesController.home);
+route.use('/search', sitesController.search);
+
+
+module.exports = route;
